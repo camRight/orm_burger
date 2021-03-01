e3a0667a070c8edffc0f18de0d79163e571dd549
Cameron Wright <cam.mac.wright@gmail.com>
2021-02-28 22:46:19 -0800

    refactor connection

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
     host: "l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    username: "rmhvnb859kt53s0m",
-    password: "ilcseuznxvxlm8te",
     port: 3306,
+    user: "rmhvnb859kt53s0m",
+    password: "ilcseuznxvxlm8te",
     database: "ksexsn9j8pte2082",
 });
 
