d68699d84ea4e96593d815324a8ac2b8eb77a515
Cameron Wright <cam.mac.wright@gmail.com>
2021-02-28 22:33:13 -0800

    commented out lines

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,5 @@
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+// const env = process.env.NODE_ENV || 'development';
+// const config = require(__dirname + '/../config/config.json')[env];
 
 
 
